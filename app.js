056f553c87bc13c024601159018671bd8e7b5b5c
Ephraim Gregor <egregor@usatoday.com>
2013-07-12 16:20:09 -0400

    Added User Add page.

--- app.js
+++ app.js
@@ -18,27 +18,43 @@ app.use(stylus.middleware({
 	compile: compile
 }))
 app.use(express.static(__dirname + '/public'))
+app.use(express.bodyParser())
+
+var log = console.log
 
 function init() {
-	var func = require('./functions.js')
+	var func = require('./functions.js'),
+		form = require('express-form')
+		field = form.field
+
+	form.configure({ autoTrim: true })
 
 	app.get('/', func.home)
 
 	// User Pages
-	app.get('/user', func.userList)
-	app.get('/user/:id', func.userDetail)
+	// app.get('/user/:id', func.userDetail)
 	app.get('/user/add', func.userAdd)
+	app.post('/user/add', 
+		form(
+			field('first').required().is(/^[a-z\.-\s]+$/i),
+			field('last').required().is(/^[a-z\.-\s]+$/i),
+			field('mes').required().is(/^[a-z]{2}\d{10}$/i).toUpper(),
+			field('expiration').required().isDate()
+		), 
+		func.userSubmit
+	)
+	app.get('/user', func.userList)
 
 	// Start app
 	app.listen(3000)
-	console.log('Starting up on port 3000.')
+	log('Starting up on port 3000.')
 }
 
 // Connect to the DB
 mongoose.connect('mongodb://localhost/prestige')
 var db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
+db.on('error', console.error.bind(console, 'Connection error:'))
 db.once('open', function callback () {
-	console.log('success!')
+	log('Connected to DB.')
 	init()
 })
--- functions.js
+++ functions.js
@@ -1,7 +1,8 @@
 var nav = {
 	Home: '/',
 	Users: '/user',
-}
+},
+	log = console.log
 
 exports.home = function(req, res) {
 	res.render('home', {title: 'Home', nav: nav, cururl: '/'})
@@ -34,6 +35,40 @@ exports.userList = function(req, res) {
 	res.render('user', {title: 'Users', nav: nav, cururl: '/user', users: users })
 }
 
-exports.userAdd = function(req, res) {}
+exports.userAdd = function(req, res) {
+	var fields = {
+		first: ['', false],
+		last: ['', false],
+		mes: ['', false],
+		expiration: ['', false]
+	}
+
+	var form = req.form,
+		err = function(field) {
+			return form.getErrors(field).length ? 'error' : '';
+		}
+
+	if(typeof form != 'undefined') {
+		fields = {
+			first: [form.first, err('first')],
+			last: [form.last, err('last')],
+			mes: [form.mes, err('mes')],
+			expiration: [form.expiration, err('expiration')]
+		}
+	}
+
+	res.render('user_add', {title: 'Add User', nav: nav, cururl: '/user', fields: fields})
+}
+
+exports.userSubmit = function(req, res) {
+	if(!req.form.isValid) {
+		exports.userAdd(req, res)
+	} else {
+		// Add validated user to DB.
+
+		// Redirect back to user list.
+		res.redirect('/user')
+	}
+}
 
 exports.userDetail = function(req, res) {}
